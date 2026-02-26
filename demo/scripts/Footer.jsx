677cdb7ffc8b16a410f7ada954d9276eb5cecb20
Xaeian <git@sqrt.pl>
2026-02-26 20:54:40 +0100

    footer

--- demo/scripts/Footer.jsx
+++ demo/scripts/Footer.jsx
@@ -12,7 +12,7 @@ const Footer = () => {
     <footer class="footer">
       <Item>© Xaeian</Item>
       <Item>Like Vanilla but JSX</Item>
-      <Item>Built with 🌰 <a class="footer-link" href="https://github.com/Xaeian/TonkaJSX">TonkaJSX</a></Item>
+      <Item>Built with<a class="footer-link" href="https://github.com/Xaeian/TonkaJSX">TonkaJSX</a></Item>
     </footer>
   );
 };
